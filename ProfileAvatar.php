2b563c4cca5bcc0e49329dfa7a78f275499d6b4a
Lukas Winkler <git@lw1.at>
2021-02-14 21:24:18 +0100

    fix replacing avatar icon
    fixes #1

--- ProfileAvatar.php
+++ ProfileAvatar.php
@@ -7,25 +7,4 @@ use Piwik\Plugin;
 class ProfileAvatar extends Plugin
 {
 
-    public function registerEvents()
-    {
-        return [
-            'Live.getExtraVisitorDetails' => 'getExtraVisitorDetails'
-        ];
-    }
-
-    public function getExtraVisitorDetails(&$result): void
-    {
-        $settings = new UserSettings();
-        $visitorID = $result["visitorId"];
-        $hash = hash("sha256", $visitorID);
-
-        if ($settings->dataURLs->getValue()) {
-            $chosenGenerator = $settings->avatarType->getValue();
-            $generator = GeneratorCollection::getGeneratorClasses($chosenGenerator,$hash);
-            $result['visitorAvatar'] = $generator->asDataUrl();
-        } else {
-            $result['visitorAvatar'] = "?module=ProfileAvatar&action=getProfileAvatar&hash=$hash";
-        }
-    }
 }
---
+++ VisitorDetails.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Piwik\Plugins\ProfileAvatar;
+
+use Piwik\Plugins\Live\VisitorDetailsAbstract;
+
+class VisitorDetails extends VisitorDetailsAbstract
+{
+    public function initProfile($visits, &$profile)
+    {
+        $settings = new UserSettings();
+        $visitorID = $profile["visitorId"];
+        $hash = hash("sha256", $visitorID);
+
+        if ($settings->dataURLs->getValue()) {
+            $chosenGenerator = $settings->avatarType->getValue();
+            $generator = GeneratorCollection::getGeneratorClasses($chosenGenerator, $hash);
+            $profile['visitorAvatar'] = $generator->asDataUrl();
+        } else {
+            $profile['visitorAvatar'] = "?module=ProfileAvatar&action=getProfileAvatar&hash=$hash";
+        }
+    }
+
+    public function extendVisitorDetails(&$visitor)
+    {
+    }
+
+}
